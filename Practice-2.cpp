d0c2a79c07ecee0ea62575f0b8866a608386bea3
Sean Beardsley <alreadysean@gmail.com>
2025-04-28 03:38:32 -0700

    Attempted a fibonacci sequence

--- Practice-2.cpp
+++ Practice-2.cpp
@@ -2,11 +2,29 @@
 
 using namespace std;
 
+int fibonacci(int N){
+
+  if (N == 0){
+    return 0;
+  }
+
+  else if (N == 1){
+    return 1;
+  }
+
+  else {
+    return fibonacci(N) + 1;
+  }
+    
+}
+
 int main(){
 
   // cout << "This was made on github." << endl;
 
   // cout << "I made this change on vs code." << endl;
+
+  cout << fibonacci(10) << endl;
 
   return 0;
 }
