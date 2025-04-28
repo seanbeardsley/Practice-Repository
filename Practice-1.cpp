c41cf3080c013540b2bd9fed0a69491ba2bcb8f2
Sean Beardsley <alreadysean@gmail.com>
2025-04-28 03:17:25 -0700

    Trying to make changes on github

--- Practice-1.cpp
+++ Practice-1.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main(){
 
-    int x, y, z;
+    int x = 1;
+    int y, z;
 
-    for(int i = 0; i < 10; i++){
-        
+    for(int i = 0; i < 5; i++){
+        x = x*2;
+        y = x*x;
+        z = y*y;
     }
+
+    cout << x << "\n" << y << "\n" << z << "\n";
     
 
     return 0;
